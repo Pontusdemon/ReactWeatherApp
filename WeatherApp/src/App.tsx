fa98d873640a5bc2dd17efd8f2e0169494a745d5
Pontusdemon <pontus.ling@gmail.com>
2026-07-29 14:52:33 +0200

    first commit

---
+++ WeatherApp/src/App.tsx
@@ -0,0 +1,40 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import './App.css'
+import Layout from './components/layout'
+import { ThemeProvider } from './components/context/theme-provider'
+import WeatherDashboard from './pages/weather-dashboard'
+import CityPage from './pages/city-page'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+function App() {
+  const queryClient = new QueryClient({
+    defaultOptions:{
+      queries:{
+        staleTime: 5 * 60 * 1000, // 5 minutes
+        gcTime: 10 * 60 * 1000, // 10 minutes
+        retry: false,
+        refetchOnWindowFocus: false,
+      }
+    }
+  })
+
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ThemeProvider defaultTheme='dark'>
+          <Layout>
+            <Routes>
+              <Route path='/' element={<WeatherDashboard />} />
+              <Route path='/city/:cityName' element={<CityPage />} />
+            </Routes>
+          </Layout>
+        </ThemeProvider>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
+}
+
+export default App
